31a41af2b4e501a1c094d904754a6fcf189a792e
alexis-brettes <alexis.brettes.iut@gmail.com>
2026-01-05 13:09:03 +0100

    Added type specs

--- Projet.ads
+++ Projet.ads
@@ -1,3 +1,17 @@
 package SGF is 
 
-   type 
+   type P_file is access file;
+
+   type file is record
+
+      nom: String;
+      droit_acces: String (1..9);
+      taille: Integer;
+      rep_parent: P_file;
+      L_enfant: P_file;
+      isRepo: Boolean;
+   
+   end record;
+
+end SGF;
+
